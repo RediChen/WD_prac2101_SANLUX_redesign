b11b0b27885bbf018f1305d2e2b82e75c7c82f33
RediiChen <a0929052958@gmail.com>
2021-01-27 21:59:42 +0800

    建立表單

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
-// $('#banner').slick({
-//     // dots: true,
-//     // autoplay: true,
-//     // autoplaySpeed: 2000,
-//     // cssEase: 'ease-in-out'
-// }) ;
+$('#banner').slick();
+/*{
+    // dots: true,
+    // autoplay: true,
+    // autoplaySpeed: 2000,
+    // cssEase: 'ease-in-out'
+} */
 $('#news-card-wrap').slick({
     slidesToShow: 3,
     cssEase: 'ease-in-out'
