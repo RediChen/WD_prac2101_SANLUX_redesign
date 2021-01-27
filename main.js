8518ce69970028a46be1df2897a4194b846808e9
RediiChen <a0929052958@gmail.com>
2021-01-27 20:22:59 +0800

    建立最新消息、報修表單

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
-$('.banner').slick({
-    // dots: true,
-    autplay: true,
-    autoplaySpeed: 2000,
+// $('#banner').slick({
+//     // dots: true,
+//     // autoplay: true,
+//     // autoplaySpeed: 2000,
+//     // cssEase: 'ease-in-out'
+// }) ;
+$('#news-card-wrap').slick({
+    slidesToShow: 3,
     cssEase: 'ease-in-out'
-}) ;
+});
