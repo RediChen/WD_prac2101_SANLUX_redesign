98bb4c627b28c4c5049d6d510ee8679dc02e7501
RediiChen <a0929052958@gmail.com>
2021-02-03 20:47:08 +0800

    更新架構&RWD(導覽列&公告)

--- main.js
+++ main.js
@@ -1,11 +1,38 @@
-$('#banner').slick();
-/*{
-    // dots: true,
-    // autoplay: true,
-    // autoplaySpeed: 2000,
-    // cssEase: 'ease-in-out'
-} */
+const tablet = 1025 ;//upper bound + 1
+const mobile = 767 ;//upper bound + 1
+const slickTime = 2500 ;
+
+$('#banner').slick(
+{
+    autoplay: true,
+    autoplaySpeed: slickTime,
+    cssEase: 'ease-in-out',
+    responsive: [
+        {
+            breakpoint: tablet,
+            settings: {
+                arrows: false
+            }
+        }]
+}) ;
 $('#news-card-wrap').slick({
+    autoplay: true,
+    autoplaySpeed: slickTime,
     slidesToShow: 3,
-    cssEase: 'ease-in-out'
+    cssEase: 'ease-in-out',
+    responsive: [
+        {
+            breakpoint: tablet,//一組分界點設定就要一對大括號
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: mobile,//不用加單位，且是小於
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }]
 });
