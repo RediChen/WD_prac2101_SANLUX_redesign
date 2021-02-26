9b6871481e4da9a538e55091874188fd62018dcd
RediiChen <a0929052958@gmail.com>
2021-02-26 22:09:54 +0800

    整體樣式修正

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ const slickTime = 2500 ;
 
 $('#banner').slick(
 {
-    autoplay: true,
-    autoplaySpeed: slickTime,
+    // autoplay: true,
+    // autoplaySpeed: slickTime,
     cssEase: 'ease-in-out',
     // dots: true,
     responsive: [
